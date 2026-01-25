f42a820fd87727943ce701a8e52b099ceecf81f3
Luke Knight <lukektx@gmail.com>
2026-01-25 16:23:58 -0600

    Shifted file structure

--- Protostar/Assets/Scripts/Objects/ShiftState.cs
+++
@@ -1,9 +0,0 @@
-using System;
-using UnityEngine;
-
-[Serializable]
-public class ShiftState
-{
-    public string Name;
-    public GameObject StateRoot;
-}
--- Protostar/Assets/Scripts/Objects/ShiftableObject.cs
+++ Protostar/Assets/Scripts/Objects/ShiftableObject.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using Game.Objects.Shift;
 
 public class ShiftableObject : MonoBehaviour, IFocusable, IEngageable, IShiftable
 {
