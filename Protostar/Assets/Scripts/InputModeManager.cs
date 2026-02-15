8c9dfc498831a9b008ea7f7ab6692eababacba0e
Luke Knight <lukektx@gmail.com>
2026-02-15 13:11:02 -0600

    mostly working controller

---
+++ Protostar/Assets/Scripts/InputModeManager.cs
@@ -0,0 +1,76 @@
+using System;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public enum InputMode
+{
+    Mouse,
+    Controller
+}
+
+public class InputModeManager : MonoBehaviour
+{
+    public static InputModeManager Instance { get; private set; }
+
+    public InputMode CurrentInputMode { get; private set; } = InputMode.Mouse;
+    public event Action<InputMode> InputModeChanged;
+
+    [Range(0f,1f)]
+    [SerializeField] private float deadzone = 0.05f;
+
+    [SerializeField] private InputActionReference navigateAction;
+    [SerializeField] private InputActionReference pointAction;
+
+    private void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    void OnEnable()
+    {
+        navigateAction.action.performed += OnNavigate;
+        pointAction.action.performed += OnPoint;
+    }
+
+    void OnDisable()
+    {
+        navigateAction.action.performed -= OnNavigate;
+        pointAction.action.performed -= OnPoint;
+    }
+
+    private void OnPoint(InputAction.CallbackContext _ctx)
+    {
+        SetInputMode(InputMode.Mouse);
+    }
+
+    private void OnNavigate(InputAction.CallbackContext ctx)
+    {
+
+        if (!(ctx.control.device is Gamepad))
+            return;
+
+        // Check what action is, ignore stick drift
+        Vector2 value = ctx.ReadValue<Vector2>();
+
+        if (value.magnitude < deadzone) return;
+
+        SetInputMode(InputMode.Controller);
+    }
+
+    private void SetInputMode(InputMode newMode)
+    {
+        if (CurrentInputMode == newMode)
+            return;
+
+        CurrentInputMode = newMode;
+        InputModeChanged?.Invoke(newMode);
+    }
+}
--- Protostar/Assets/UI Toolkit/Panels/StartViewPresenter.cs
+++ Protostar/Assets/UI Toolkit/Panels/StartViewPresenter.cs
@@ -1,14 +1,19 @@
 using System;
 using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.InputSystem.LowLevel;
 using UnityEngine.UIElements;
 
 public class StartViewPresenter : MonoBehaviour
 {
     public static StartViewPresenter Instance { get; private set; }
-    
+
+    private const string CONTROLLER_MODE_CLASS = "controller-mode";
+    private const string MOUSE_MODE_CLASS = "mouse-mode";
+
     [SerializeField] private string gameSceneName = "MainLevel";
     [SerializeField] private string mainMenuSceneName = "MainMenu";
-    
+
     private VisualElement root;
     private VisualElement settingsView;
     private VisualElement mainMenuView;
@@ -40,18 +45,72 @@ public class StartViewPresenter : MonoBehaviour
         SetupControlsMenu();
 
         GameStateManager.Instance.OnStateChanged += OnGameStateChanged;
+        InputModeManager.Instance.InputModeChanged += OnInputModeChanged;
         OnGameStateChanged(GameStateManager.Instance.CurrentState);
 
+        var button = mainMenuView.Q<Button>();
+
         // Force initial focus after UI has fully initialized
-        root.schedule.Execute(() => mainMenuView.Q<Button>()?.Focus()).StartingIn(500);
+        void OnGeometryChanged(GeometryChangedEvent evt)
+        {
+            button.UnregisterCallback<GeometryChangedEvent>(OnGeometryChanged);
+
+            root.panel?.visualTree.Focus();
+            button.Focus();
+
+            //Debug.Log("Focused after layout: " + root.panel?.focusController?.focusedElement);
+        }
+
+        button.RegisterCallback<GeometryChangedEvent>(OnGeometryChanged);
+
+        // default to mouse input mode
+        SetMouseMode();
     }
+
 
     private void OnDestroy()
     {
+        if(InputModeManager.Instance != null)
+        {
+            InputModeManager.Instance.InputModeChanged -= OnInputModeChanged;
+        }
         if (GameStateManager.Instance != null)
         {
             GameStateManager.Instance.OnStateChanged -= OnGameStateChanged;
         }
+    }
+
+    private void OnInputModeChanged(InputMode inputMode)
+    {
+        switch (inputMode)
+        {
+            case InputMode.Mouse:
+                SetMouseMode();
+                break;
+            case InputMode.Controller:
+                SetControllerMode();
+                break;
+        }
+    }
+
+    private void SetControllerMode()
+    {
+        // If already in controller mode, dont need to switch
+        if(root.ClassListContains(CONTROLLER_MODE_CLASS)) return;
+
+        //Debug.Log("Set to controller mode");
+        root.RemoveFromClassList(MOUSE_MODE_CLASS);
+        root.AddToClassList(CONTROLLER_MODE_CLASS);
+    }
+
+    private void SetMouseMode()
+    {
+        // If already in controller mode, dont need to switch
+        if (root.ClassListContains(MOUSE_MODE_CLASS)) return;
+
+        //Debug.Log("Set to mouse mode");
+        root.RemoveFromClassList(CONTROLLER_MODE_CLASS);
+        root.AddToClassList(MOUSE_MODE_CLASS);
     }
 
     private void SetupMainMenu()
